4a628e46beaa0ac6931713d36d85e3f80454ff8c
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-02 18:13:17 +0300

    order field added to the model for ordering and reordering objects in the future

--- myApp/models.py
+++ myApp/models.py
@@ -5,6 +5,18 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=264)
     is_complete = models.BooleanField(default=False)
+    order = models.PositiveBigIntegerField(default=0, blank=True, null=True)
+    
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+        
+        if is_new and self.order is None:
+            self.order = self.pk
+            super().save(update_fields=['order'])
