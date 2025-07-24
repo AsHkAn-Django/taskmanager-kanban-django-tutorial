b311990d07f41d71c628c7fe027e889a38ed2987
Ashkan <ashkan_ahrari@yahoo.com>
2025-07-24 18:48:39 +0300

    add registration

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 app_name = 'users'
@@ -7,4 +8,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    model = get_user_model
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = "users/signup.html"
