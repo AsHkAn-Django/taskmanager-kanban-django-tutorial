210ed8abe4a587521a6e72b7662cbf1bfd6eb069
Ashkan <ashkan_ahrari@yahoo.com>
2025-07-24 18:31:28 +0300

    add login and logout to main page

--- taskManager/urls.py
+++ taskManager/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('myApp.api.routers')),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('myApp.urls')),
 ]
