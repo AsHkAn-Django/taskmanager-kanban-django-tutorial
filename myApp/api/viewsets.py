b2b4867710f21660ed62fc95987dde7009abd45e
Ashkan <ashkan_ahrari@yahoo.com>
2025-07-25 17:44:47 +0300

    add move_up and move_down actions for reordering the tasks in the api list

--- myApp/api/viewsets.py
+++ myApp/api/viewsets.py
@@ -34,6 +34,35 @@ class TaskViewSet(viewsets.ModelViewSet):
         task2.save()
         return Response({"message": f"'{task1.name}' has been swapped with '{task2.name}'!"}, status=status.HTTP_200_OK)
 
+    @action(detail=True, methods=['get'])
+    def move_up(self, request, pk=None):
+        """Move the task one step higher in the list."""
+        task = self.get_object()
+        task_order = task.order
+        tasks_above = Task.objects.filter(user=task.user, order__lt=task_order).order_by('-order')
+        if not tasks_above:
+            return Response({"error": f"'{task.name}' is already on top of the list."}, status=status.HTTP_400_BAD_REQUEST)
+
+        chosen_task = tasks_above.last()
+        task.order, chosen_task.order = chosen_task.order, task.order
+        task.save()
+        chosen_task.save()
+        return Response({"success": f"'{task.name}' went one step up."}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['get'])
+    def move_down(self, request, pk=None):
+        """Move the task one step higher in the list."""
+        task = self.get_object()
+        task_order = task.order
+        tasks_under = Task.objects.filter(user=task.user, order__gt=task_order).order_by('order')
+        if not tasks_under:
+            return Response({"error": f"'{task.name}' is already at the bottom of the list."}, status=status.HTTP_400_BAD_REQUEST)
+
+        chosen_task = tasks_under.first()
+        task.order, chosen_task.order = chosen_task.order, task.order
+        task.save()
+        chosen_task.save()
+        return Response({"success": f"'{task.name}' went one step down."}, status=status.HTTP_200_OK)
 
 class UserListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
